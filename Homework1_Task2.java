13468932816ca35bac001e465b71a3cb40cc2aaa
Dmitri <datitov2009@yandex.ru>
2022-12-13 18:25:22 +0300

    refactor Task2

--- Homework1_Task2.java
+++ Homework1_Task2.java
@@ -17,11 +17,11 @@ public class Homework1_Task2 {
     public static int removeElement(int[] nums, int val) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == val) {
-                continue;
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;;
             }
-            nums[count] = nums[i];
-            count++;
+
         }
         return count;
     }
