31b10d13c535951799bc6d3fe839f1463f8e6083
Dmitri <datitov2009@yandex.ru>
2022-12-15 17:56:15 +0300

    add Homework2 Task1 file

--- Homework2_Task1.java
+++ Homework2_Task1.java
@@ -1,34 +1,42 @@
 public class Homework2_Task1 {
     public static void main(String[] args) {
-        int [][] grid = {{0,0,1,0,0,0,0,1,0,0,0,0,0}, {0,0,0,0,0,0,0,1,1,1,0,0,0},
-            {0,1,1,0,1,0,0,0,0,0,0,0,0}, {0,1,0,0,1,1,0,0,1,0,1,0,0}, {0,1,0,0,1,1,0,0,1,1,1,0,0},
-            {0,0,0,0,0,0,0,0,0,0,1,0,0},{0,0,0,0,0,0,0,1,1,1,0,0,0},{0,0,0,0,0,0,0,1,1,0,0,0,0}};
-        System.out.println(maxAreaOfIsland(grid));
+        int[][] island = new int[][]{{0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0}, {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+        System.out.println(maxAreaOfIsland(island));
+
     }
-    public static int maxAreaOfIsland(int[][] grid) {
-        int count = 0;
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == 1) {
-                    dfs(i, j, grid);
-                    count++;
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) {
+                    continue;
+                }
+                int cur = dfs(grid, i, j);
+                if (cur > max) {
+                    max = cur;
                 }
             }
         }
-        return count;
+        return max;
     }
 
-    private static void dfs(int i, int j, int[][] grid) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length
-                || grid[i][j] != 1) {
-            return;
-        }
+    int[][] island = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-        grid[i][j] = 0;
-        dfs(i - 1, j, grid);
-        dfs(i + 1, j, grid);
-        dfs(i, j - 1, grid);
-        dfs(i, j + 1, grid);
+    public int dfs(int[][] grid, int row, int column) {
+        if (row < 0 || row >= grid.length || column < 0 || column >= grid[0].length ||
+                grid[row][column] == 0) {
+            return 0;
+        }
+        int res = 1;
+        grid[row][column] = 0;
+        for (int i = 0; i < island.length; i++) {
+            res += dfs(grid, row + island[i][0], column + island[i][1]);
+        }
+        return res;
     }
 }
+
 
